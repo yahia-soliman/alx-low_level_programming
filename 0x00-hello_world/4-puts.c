1add0ee446343d26145f94ecd6ad42165817e821
Yahia Soliman <yn7599@gmail.com>
2023-07-16 16:10:20 +0300

    checking 0-4 tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ *
+ * This program prints an important message
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    Puts("\"Programming is like building a multilingual puzzle\n")
+
+    return 0
+}
