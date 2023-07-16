437c5265823842486bafc7a5ad1682c3347fc662
Yahia Soliman <yn7599@gmail.com>
2023-07-16 17:40:26 +0300

    trying to figure out WHY

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 /**
- *
  * This program prints an important message
- *
+ * under GG public license
  */
 
 #include <stdio.h>
 
+/**
+ * main function is the starting point of the program.
+ * returns : a srting!
+ */
 int main(void)
 {
-    Puts("\"Programming is like building a multilingual puzzle\n")
-
-    return 0
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
