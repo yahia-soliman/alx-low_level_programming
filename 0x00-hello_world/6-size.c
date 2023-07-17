fdbf65f06d644ca2b57ee47d23f7bd93406f3f9e
Yahia Soliman <yn7599@gmail.com>
2023-07-17 18:59:05 +0300

    fix\for\betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-/*  This program prints an important message */
+/*  This program prints size of various types. */
 
 #include <stdio.h>
 
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %li byte(s)\n",sizeof(char));
-	printf("Size of an int: %li byte(s)\n",sizeof(int));
-	printf("Size of a long int: %li byte(s)\n",sizeof(long int));
-	printf("Size of a long long int: %li byte(s)\n",sizeof(long long int));
-	printf("Size of a float: %li byte(s)\n",sizeof(float));
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
