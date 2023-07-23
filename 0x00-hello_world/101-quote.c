bac3d7c399d350d1c443bc4e4bbbd49f7f0290d5
Yahia Soliman <yn7599@gmail.com>
2023-07-23 22:55:07 +0300

    quote: "Hello, Advanced!"

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, sizeof(char), sizeof(str), stderr);
+	fwrite(str, sizeof(char), sizeof(str) - 1, stderr);
 	return (1);
 }
