85d4106d951f4fdf013fb215d472627762b27d52
Yahia Soliman <yn7599@gmail.com>
2023-07-23 22:51:43 +0300

    Hello, Advanced!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - you know what it is
+ * Return: 1
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, sizeof(char), sizeof(str), stderr);
+	return (1);
+}
