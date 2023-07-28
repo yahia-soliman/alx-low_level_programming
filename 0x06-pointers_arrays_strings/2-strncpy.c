d4cf6c8aa971cb0779fb330c123b2496cc3b7214
Yahia Soliman <yn7599@gmail.com>
2023-07-29 02:03:18 +0300

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,15 @@
 /**
  * _strncpy - copies at most n characters
- * ِِ@src: the source it yanks from
+ * @src: the source it yanks from
  * @dest: the destination it puts into
+ * @n: number of chars fo copy
+ * Return: updated @dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && dest[i] != '\0')	
+	while (i < n && dest[i] != '\0')
 	{
 		*(dest + i) = src[i];
 		i++;
