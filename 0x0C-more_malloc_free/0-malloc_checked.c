8afbaa02643e81e317ecce9b79d9b9bd6c6e2c7f
Yahia <yn7599@gmail.com>
2023-08-10 20:39:21 +0300

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - malloc with process termination
+ * @b: number of bytes to allocate
+ * Return: pointer to first byte in the allocation
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *r = malloc(b);
+
+	if (r == NULL)
+		exit(98);
+	
+	return (r);
+}
