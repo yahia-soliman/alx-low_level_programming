7c4acfa9a149e3e6a3c66092149fde8eb2738765
Yahia <yn7599@gmail.com>
2023-08-15 00:13:22 +0300

    NULL dawg?!

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initializes a dog informations to a struct dog
  * @d: pointer to struct dog
@@ -8,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,27 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_dog - prints the dog name, age, owenr
  * @d: address of the dog's struct dog varialble
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %0.6f\nOwner: %s\n"
-			, (*d).name, (*d).age, (*d).owner);
+if (d == NULL)
+	putchar('\0');
+else
+{
+	if ((*d).name == NULL)
+		printf("Name: %s\n", "(nil)");
+	else
+		printf("Name: %s\n", (*d).name);
+	if ((*d).age == 0.0)
+		printf("Age: %s\n", "(nil)");
+	else
+		printf("Age: %0.6f\n", (*d).age);
+	if ((*d).owner == NULL)
+		printf("Owner: %s\n", "(nil)");
+	else
+		printf("Owner: %s\n", (*d).owner);
+}
 }
