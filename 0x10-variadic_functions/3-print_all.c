9d31ba25ec0ff55a790486c1f51a62fca0dbb355
Yahia Soliman <yn7599@gmail.com>
2023-08-18 20:45:50 +0300

    task 3 first try

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,85 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+void pc(va_list args);
+void pi(va_list args);
+void pf(va_list args);
+void ps(va_list args);
+/**
+ * print_all - prints anything with any type
+ * @format:    string of given argument type
+ * {i, c, f, s} for {int, char, float, string}
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int x, y;
+	void (*ptrf)(va_list);
+	func arr[4] = {
+		{'c', pc},
+		{'i', pi},
+		{'f', pf},
+		{'s', ps},};
+
+	va_start(args, format);
+	x = 0;
+	while (format[x] && format)
+	{
+		y = 0;
+		while (y < 4)
+		{
+			if (arr[y].c == format[x])
+			{
+				ptrf = arr[y].f;
+				ptrf(args);
+			}
+			y++;
+		}
+		x++;
+	}
+	printf("\n");
+}
+
+/**
+ * pc -   prints a character
+ * @args: variadic args list
+ */
+void pc(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * pi -   prints an  integer
+ * @args: variadic args list
+ */
+void pi(va_list args)
+{
+	printf("%i", va_arg(args, int));
+}
+
+/**
+ * pf -   prints a     float
+ * @args: variadic args list
+ */
+void pf(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * ps -   prints a    string
+ * @args: variadic args list
+ */
+void ps(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	if (s)
+	{
+		printf("%s", s);
+		return;
+	}
+	printf("(nil)");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VAR_FUN_HEADER
 #define VAR_FUN_HEADER
 
+#include <stdarg.h>
+/**
+ * func - struct of printfunction according to a character
+ * @c:    char data type
+ * @f:    pointer to suitable function
+ */
+typedef struct func
+{
+	char c;
+	void (*f)(va_list);
+} func;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
