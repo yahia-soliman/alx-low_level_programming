3cb0dc2115e1adf5637f02db366c53dfa253fbcb
Yahia Soliman <yn7599@gmail.com>
2023-10-16 17:38:25 +0300

    abc to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 /**
  * _atoi - converts a string to an integer
  * @s: the string to convert
@@ -9,7 +8,7 @@ int _atoi(char *s)
 	int total = 0, signal = 1, i = 0;
 
 	if (!s)
-		return 0;
+		return (0);
 
 	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
@@ -24,27 +23,4 @@ int _atoi(char *s)
 		i++;
 	}
 	return (total * signal);
-}
-
-int main(void)
-{
-    int nb;
-
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("12313123214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
-    return (0);
 }
