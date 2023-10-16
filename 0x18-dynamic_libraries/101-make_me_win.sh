57b2718b40611d266e46ffda5ad30b14f931a044
Yahia Soliman <yn7599@gmail.com>
2023-10-17 00:08:14 +0300

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/99yh/alx-low_level_programming/main/0x18-dynamic_libraries/fakeio.so --output /tmp/fakeio.so
+curl -s --output /tmp/fakeio.so https://raw.githubusercontent.com/99yh/alx-low_level_programming/main/0x18-dynamic_libraries/fakeio.so
 export LD_PRELOAD=/tmp/fakeio.so
