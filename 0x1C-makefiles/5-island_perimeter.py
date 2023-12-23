17079ec07e265b01ec898ef594309a82c4d4766f
Yahia <yn7599@gmail.com>
2023-12-23 17:45:19 +0200

    full make

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,9 +15,12 @@ def cell_water_border(grid, row, cell):
     """Get how many sides of a cell connected to water."""
     if grid[row][cell] == 0:
         return 0
-    return 4 - (
-        grid[row - 1][cell] +
-        grid[row + 1][cell] +
-        grid[row][cell - 1] +
-        grid[row][cell + 1]
-    )
+    try:
+        return 4 - (
+                grid[row - 1][cell] +
+                grid[row + 1][cell] +
+                grid[row][cell - 1] +
+                grid[row][cell + 1]
+                )
+    except IndexError:
+        return 0
