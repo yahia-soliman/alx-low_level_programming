ffe98bfef27a45f7b634930cc1671cb368d6d9cd
Yahia <yn7599@gmail.com>
2024-01-24 13:10:32 +0200

    Made some Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Calculate the perimiter for an island."""
+
+
+def island_perimeter(grid):
+    """Get the perimeter of the grid."""
+    perimeter = 0
+    for row in range(len(grid)):
+        for cell in range(len(grid[row])):
+            if grid[row][cell] == 0:
+                continue
+            try:
+                perimeter += 4 - (
+                    grid[row - 1][cell] +
+                    grid[row + 1][cell] +
+                    grid[row][cell - 1] +
+                    grid[row][cell + 1]
+                )
+            except IndexError:
+                return 0
+    return perimeter
